0795bc5cf03a6c74d7d3310dce5d41efe6e3e5dc
George Robinson Jr <george.robinson0930@gmail.com>
2025-06-07 02:24:28 -0400

    113_first_assign

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.PostListView.as_view(), name="list"),
     path("create/", views.PostCreateView.as_view(), name="create"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="detail"),
-    path("<int:pk>/", views.PostUpdateView.as_view(), name="edit"),
-    path("<int:pk>/", views.PostDeleteView.as_view(), name="delete"),
+    path("<int:pk>/edit/", views.PostUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="delete"),
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -7,25 +7,38 @@ from django.views.generic import (
 )
 from .models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
     model = Post
     fields = [
-        "title", "subtitle", "body", "author"
+        "title", "subtitle", "body", "status"
     ]
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "posts/delete.html"
     model = Post
     success_url = reverse_lazy("list")
 
-class PostCreateView(CreateView):
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/create.html"
     model = Post
     fields = [
-        "title", "subtitle", "body", "author"
-    ] 
+        "title", "subtitle", "body", "status"
+    ]
+
+    def form_valid(self, form): 
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class PostListView(ListView):
     template_name = "posts/list.html"
@@ -34,6 +47,8 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
     model = Post
+
+
 
     
  
